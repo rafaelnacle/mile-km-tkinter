4ec4d032da6f542a3306ce1a4e833ec17ba0a500
= <=>
2025-03-13 13:07:23 -0300

    Basic layout of the conversor

--- main.py
+++ main.py
@@ -3,7 +3,25 @@ from tkinter import *
 window = Tk()
 
 window.title("Mile to Km converter")
-window.minsize(width=400, height=200)
-window.config(padx=10, pady=10)
+window.minsize(width=200, height=100)
+window.config(padx=20, pady=20)
+
+mile_label = Label(text="Miles")
+mile_label.grid(column=2, row=1)
+
+mile_input = Entry(width=20)
+mile_input.grid(column=1, row=1)
+
+is_equal_text = Label(text="Is equal to")
+is_equal_text.grid(column=0,row=2)
+
+km_value_text = Label(text="0")
+km_value_text.grid(column=1,row=2)
+
+km_text = Label(text="Km")
+km_text.grid(column=2,row=2)
+
+calculate_button = Button(text="Calculate")
+calculate_button.grid(column=1, row=3)
 
 window.mainloop()
